07a0d89f1515ef1007a7e844d945aad82bde4715
Cecilia Ehrlichman <cehrlichman@Cecilias-MacBook-Pro.local>
2024-04-17 15:58:24 -0500

    update player

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from linparse import *
-import random
+import random, math
 from datetime import datetime
 from score import calculate_score
 '''
@@ -8,20 +8,25 @@ table_id to Table object
 running_tables = {}
 game_counter = 0
 
-
+def update_game_state():
+    '''
+    Call after most functions to send an updatated game state to the fromt-end.
+    '''
+    pass
 
 class Game:
 
-    def __init__(self, players, seed = None):
+    def __init__(self, players, table_id, seed = None):
         if not seed:
             seed = int(datetime.now().timestamp())
         self.game_random = random.Random(seed)
         self.current_hand = BridgeHand(players, dealer = None, hands = {}, bids = [], play = None, contract = None, declarer = None, doubled = None, vuln = None, made = None, claimed = None)
         self.game_phase = "AUCTION"
-        self.id = None
-
+        self.game_id = None
+        self.table_id = table_id
         self.deal()
         self.set_dealer()
+        self.current_player = self.current_hand.dealer
 
     def deal(self):
         full_hand = full_hand()
@@ -38,18 +43,68 @@ class Game:
             'W': W_hand
         }
         
-    def new_game(North = None, East = None, South = None, West = None):
+    def new_game(self, North = None, East = None, South = None, West = None):
+        
         '''
         Get a new game id.
-        Create plaglobal game_counteryer dictionary.
+        Create player global game_counter dictionary.
         Set the dealer and vulnerability.
         '''
                 
+    def begin_play_phase(self):
+        self.game_phase = "PLAY"
 
-    def play_card():
-        pass
+    def update_current_player(self):
+        ''' 
+        Check that the card is in the players hand.
+        Make sure it is the players turn.
+        See if it is the opening lead, if so make sure the right player is leading.
+        If the trick has been started, check that the person is following suit.
+        Add the card they played to the play dictionary. 
+        Remove that card from their hand. 
+        Update game state.
+        '''
+        # Check if the game is over
+        if len(self.current_hand.play) == 13 and len(self.current_hand.play[-1]) == 5:
+            self.current_player = None
+            # Tell front end that game is over
+            return
+        
+        # Check if this is the opening lead
+        if len(self.current_hand.play) == 0:
+            self.current_player = self.get_left_player(self.current_hand.declarer)
+            return
 
-    def make_bid(player, bid):
+        # check if a trick is in progress
+        if len(self.current_hand.play[-1]) < 5:
+            self.current_player = self.get_left_player(self.current_player)
+            return
+        
+        # if we are starting a new trick, see who won the last trick
+        last_trick = self.current_hand.play[-1]
+        trick = {}
+        trick['N'] = last_trick['N']
+        trick['E'] = last_trick['E']
+        trick['S'] = last_trick['S']
+        trick['W'] = last_trick['W']
+        self.current_player = get_trick_winner(trick, last_trick['lead'], trump=self.current_hand.contract[1])
+        return
+
+    def get_left_player(self, player):
+        return PLAYERS[PLAYER_MAP[player] + 1 % 4]
+
+    def play_card(self, player: str, card: Card):
+        '''
+        input:
+            player: str (direction)
+            card: Card 
+        '''
+        # check if the card is in the players hand
+        if not card in self.current_hand.hands[player]:
+            return False
+
+
+    def make_bid(self, player, bid):
         '''
         Check if the bid is valid.
         If so, update the BridgeHand auction state.
@@ -57,7 +112,7 @@ class Game:
         '''
         pass
 
-    def end_game():
+    def end_game(self):
         '''
         Distroy stored game state.
         Calculate and output score.
@@ -81,8 +136,9 @@ class Game:
                            'both', 'none', 'NS', 'EW']
         self.current_hand.vuln = vulnerabilities[running_tables[table_id].game_count % 16]
     
-    def get_score(vulerable, contract, result):
-        return calculate_score(contract[0][0], contract[0][1], contract[1], result)
+    def get_score(self, vulerable, contract, result):
+        pass
+        # return calculate_score(contract[0][0], contract[0][1], contract[1], result)
 
 
 class Table:
@@ -92,7 +148,7 @@ class Table:
     players: dict (keys: positions)
     seed: int
     '''
-    def __init__(self, players, seed = None):
+    def __init__(self, players: dict, seed: int = None):
         self.players = players
         self.seed = seed
         self.game_count = 0
@@ -100,9 +156,12 @@ class Table:
         self.EW_score = 0
         self.current_game = None
         self.game_id_list = []
+        self.table_id = math.trunc(int(datetime.now().timestamp()))
 
-    def new_game():
-        pass
+    def new_game(self):
+        game_id = self.game_id = math.trunc(int(datetime.now().timestamp()))
+        self.game_id_list.append(game_id)
+        self.current_game = Game(self.players, game_id, seed = self.seed)
     
     def join_table(self):
         pass
@@ -111,7 +170,10 @@ class Table:
         pass
 
 if __name__=="__main__": 
-    pass
-    my_game = Game()
-    # my_game.deal()
-    # print(parse_linfile("example.lin").bids)
+    # pass
+    # my_game = Game(["1", "2", "3", "4"], 0)
+    bridge_hand = parse_linfile("example.lin")
+    print(bridge_hand.players)
+    print(bridge_hand.play)
+    print(bridge_hand.hands)
+    # my_game.get_score(bridge_hand.vuln, bridge_hand.contract, bridge_hand.made)
