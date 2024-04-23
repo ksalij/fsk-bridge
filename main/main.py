46f8e2efe95a3f7cd6f054e4ef8aa99b079610fa
oTullio <oliver@tullio.net>
2024-04-22 23:24:24 -0500

    loading in images!

--- main/main.py
+++ main/main.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, jsonify, url_for, render_template, send_from_directory
 from _thread import *
 import random
 import json
@@ -8,8 +7,9 @@ import time
 import threading
 import sys
 import urllib
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 app_data = {
     "name": "Peter's Starter Template for a Flask Web App",
@@ -32,6 +32,29 @@ def openTable(playerID):
 @app.route('/startGame/<tableID>/<seat>')
 def watchgame(tableID, seat):
     return json.dumps("{} is ready to start!".format(seat))
+
+@app.route('/get_image_list')
+def get_image_list():
+    print('Getting image list!', file=sys.stderr)
+    image_folder = 'static/cardimages/'  # Your image folder path
+    image_list = os.listdir(image_folder)
+    print("Image list: " + str(image_list), file=sys.stderr)
+    return jsonify(image_list)
+
+@app.route('/your-flask-endpoint')
+def get_image_urls():
+    image_folder = 'static/cardimages/'  # Your image folder path
+    image_list = os.listdir(image_folder)
+    # Prepend the static URL to each filename
+    print(image_list, file=sys.stderr)
+    image_urls = [url_for('static', filename=filename) for filename in image_list]
+    return jsonify(image_urls)
+
+
+@app.route('/your-flask-endpoint/static/<path:filename>')
+def get_image(filename):
+    return send_from_directory('static', 'cardimages/' + filename)
+
 
 if __name__ == '__main__':
     my_port = 5000
--- main/static/js/index.js
+++ main/static/js/index.js
@@ -28,9 +28,9 @@ var gameInfoDiv = document.getElementById('players');
 gameInfoDiv.innerHTML = (
     "<p>Player Names: " + JSON.stringify(jsonData.playerNames) + "</p>"
     + "<br>"
-    + "<button onclick=displayPlayers()>Click me!</button>"
+    + "<button onclick=displayPlayers()>Click me to display players!</button>"
     // put game display stuff here!
-    
+    + "<button onclick=displayAllCards()>Click me to display cards!</button>"
 );
 
 function displayPlayers() {
@@ -40,8 +40,123 @@ function displayPlayers() {
     );
 }
 
+// function displayAllCards() {
+//     var logPrintingDiv = document.getElementById('logPrinting');
+
+//     // $.ajax({
+//     //     url: '/get_image_list',
+//     //     success: function(data) {
+//     //         $.each(data, function(index, filename) {
+//     //             $("#allCards").append("<img src='/static/cardimages/" + filename + "'>");
+//     //         });
+//     //     }
+//     // });
+
+//     logPrintingDiv.innerHTML = (
+//         "<p>DisplayAllCards called!</p>"
+//     );
+
+//     $.get('/static/cardimages', function(data) {
+//         $(data).find("a").attr("href", function(i, val) {
+//             $("allCards").append("<img src='/static/cardimages/" + val + "'>");
+//         });
+//     });
+
+//     logPrintingDiv.innerHTML += (
+//         "<p>DisplayAllCards successful!</p>"
+//     );
+
+//     // var folder = "{{url_for('static', filename='cardimages')}}";
+
+//     // $.ajax({
+//     //     url: folder,
+//     //     success: function(data) {
+//     //         $(data).find("a").attr("href", function(i, val) {
+//     //             $("body").append("<img src='" + folder + '/' + val + "'>");
+//     //         });
+//     //     }
+//     // });
+// }
 
 
-// Add more lines like these to display other information
+
+// Function to fetch JSON data from the Flask endpoint
+// function displayAllCards() {
+//     fetch('/get_image_list')
+//     .then(response => response.json())
+//     .then(data => {
+//         // Call function to append images to HTML div
+//         appendImages(data.images);
+//     })
+//     .catch(error => {
+//         console.error('Error fetching images:', error);
+//     });
+// }
+
+// // Function to append images to HTML div
+// function appendImages(imageList) {
+//     const div = document.getElementById('allCards');
+
+//     imageList.forEach(imageFilename => {
+//         const img = document.createElement('img');
+//         img.src = imageFilename;
+//         div.appendChild(img);
+//     });
+// }
+
+// // Fetch the JSON data from the Flask endpoint
+// function displayAllCards() {
+//     fetch('/get_image_list')
+//     .then(response => response.json())
+//     .then(data => {
+//         // Select the HTML div where you want to append the images
+//         const div = document.getElementById('allCards');
+
+//         // Loop through each image filename in the JSON data
+//         data.forEach(filename => {
+//         // Create a new <img> element
+//         const img = document.createElement('img');
+
+//         // Set the src attribute of the <img> element to the filename
+//         img.src = "{{url_for('static', filename='" + filename + "')}}"
+
+//         // Append the <img> element to the HTML div
+//         div.appendChild(img);
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Error fetching JSON:', error);
+//     });
+// }
 
 
+// Function to preload images
+function preloadImages(imageUrls) {
+    imageUrls.forEach(url => {
+      const fullUrl = `/your-flask-endpoint/${url}`;
+
+      console.log("whyyyy")
+
+      const img = new Image();
+      img.src = fullUrl;
+      //img.style.display = 'none'; // Hide the image
+      document.body.appendChild(img); // Append to body to trigger loading
+    });
+}
+  
+function fetchImages(){
+    // Fetch the image URLs from the Flask endpoint
+    fetch('/your-flask-endpoint')
+        .then(response => response.json())
+        .then(data => {
+        // Preload the images
+        preloadImages(data);
+        })
+        .catch(error => {
+        console.error('Error fetching image URLs:', error);
+        });
+}
+
+    
+// // Call the fetchImages function when the page loads
+window.onload = fetchImages();
