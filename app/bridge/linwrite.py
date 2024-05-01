a2d8581e3695813320557bed0a969416983c5866
ksalij <salijk@carleton.edu>
2024-05-01 15:03:03 -0500

    added hands to linwrite.py

--- app/bridge/linwrite.py
+++ app/bridge/linwrite.py
@@ -2,16 +2,23 @@ from linparse import *
 from server import *
 
 DEALER_MAP = {'S':1, 'W':2, 'N':3, 'E':4}
+SUITS = {0:'C', 1:'D', 2:'H', 3:'S'}
+RANK_NAMES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 
 def linwrite(table: Table):
     bridge_hand = table.current_game.current_bridgehand
     output = ''
     output += 'pn|' + bridge_hand.players['S'] + ',' + bridge_hand.players['W'] + ',' + bridge_hand.players['N'] + ',' + bridge_hand.players['E'] + '|st||md|'
     output += str(DEALER_MAP[bridge_hand.dealer])
-    # hands
-    print(bridge_hand.hands['S'].sort().reverse())
-    for card in bridge_hand.hands['S'].sort().reverse():
-        ouput += card.suitname + card.rankname
+
+    for dir in bridge_hand.hands:
+        if dir == 'E':
+            break
+        cards_str = 'S' + ''.join(RANK_NAMES[card.rank-2] for card in bridge_hand.hands[dir] if card.suitname == 'S') + \
+                    'H' + ''.join(RANK_NAMES[card.rank-2] for card in bridge_hand.hands[dir] if card.suitname == 'H') + \
+                    'D' + ''.join(RANK_NAMES[card.rank-2] for card in bridge_hand.hands[dir] if card.suitname == 'D') + \
+                    'C' + ''.join(RANK_NAMES[card.rank-2] for card in bridge_hand.hands[dir] if card.suitname == 'C')
+        output += cards_str + ','
 
     vuln = {'NS': 'n','WE': 'e','none': 'o', 'both': 'b'}
     output += '|rh||ah|Board ' + str(table.game_count) + '|sv|' + vuln[bridge_hand.vuln]
