b7ca13ac83dc9cea2b2d19e2f97b444358d6a042
oTullio <oliver@tullio.com>
2024-05-14 13:38:33 -0500

    fixed password confirm eye icon

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -10,3 +10,16 @@ document.getElementById('eye').addEventListener('click', function() {
       icon.name = 'eye-outline';
     }
 });
+
+document.getElementById('eye2').addEventListener('click', function() {
+  var passwordInput = document.getElementById('confirm');
+  var icon = document.getElementById('eye2');
+  
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    icon.name = 'eye-off-outline';
+  } else {
+    passwordInput.type = 'password';
+    icon.name = 'eye-outline';
+  }
+});
