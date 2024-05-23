b6678fc1034e0a67bf360b1779f953d2ed41bc27
Warren Kozak <kozakw@carleton.edu>
2024-05-22 22:07:45 -0500

    Colorize chat and other chat fixes

--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -1,12 +1,56 @@
 $(document).ready(function(){
 
-  //var socket = io.connect('http://localhost:80');
+  var numOfMessages = 0;
+  var date = new Date().getTime();
+  var averages = [10000, 10000, 10000, 10000, 10000];
+
+  var timeLimit = 0;
+  var muteTime;
 
   $('#send').on('click', function() {
-    //user = document.getElementById('username').innerHTML;
     user = username;
-    socket.emit("sendMessage", user, document.getElementById('textInput').value, window.location.pathname.split("/")[2]);
+    let message = document.getElementById('textInput').value;
+    if (message === '') {
+        return;
+    }
+	
+    // Clear the text input feild
     document.getElementById('textInput').value = '';
+    
+
+    // Rate limiter
+    var currentTime = new Date().getTime();
+
+    if (timeLimit !== 0) {
+	if (currentTime - muteTime > timeLimit) {
+	    timeLimit = 0;
+	} else if (currentTime - muteTime <= timeLimit) {
+	    return;
+	}
+    }
+
+    var sepTime = currentTime - date;
+    date = currentTime;
+
+    averages.push(sepTime);
+    averages.shift();
+
+    var total = 0;
+    for (var i = 0; i < averages.length; i++) {
+        total += averages[i];
+    }
+    var avgTime = total / averages.length;
+
+    // If messages are being sent too fast
+    if (avgTime <= 1000) {
+	socket.emit("sendMessage", 'server', user + ', you are typing too quickly! You have been muted for 10 seconds.', window.location.pathname.split("/")[2]);
+	timeLimit = 10000;
+	muteTime = new Date().getTime();
+	return;
+    }
+    
+    // Emit the user's chat
+    socket.emit("sendMessage", user, message, window.location.pathname.split("/")[2]);
   });
 
   socket.on('updateChat', (user, response) => {
@@ -16,11 +60,26 @@ $(document).ready(function(){
     if (user == username) {
       newText.id = "currentUserChat";
     } else if (user == "server") {
-      newText.id = "serverChat"
+      newText.id = "serverChat";
+    } else if (user == "none") {
+      newText.id = "none";
+      newText.innerHTML = response;
     } else {
       newText.id = "userChat";
     }
-    newText.innerHTML = user + ": " + response;
+
+    // color the usernames
+    if (user === "none") {
+        newText.innerHTML = response;
+    } else {
+        newText.innerHTML = user + ": " + response;
+    }
+    
+    if (user !== 'server') {
+        var chatColor = colorTag(user).hex();
+        console.log(chatColor);
+        newText.style.color = '#' + chatColor;
+    }
 
     newTextContainer = document.createElement("div");
     newTextContainer.id = "chatContainer"
@@ -38,8 +97,4 @@ $(document).ready(function(){
     }
   });
 
-  $('#gameState').on('click', function() {
-    
-  })
-      
 });
