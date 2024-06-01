bcdc3183b5a18f3c8140ca81fba96fc0a2567375
ehrlichmanc <ehrlichmanc@carleton.edu>
2024-06-01 00:17:38 -0500

    fix

--- app/app.py
+++ app/app.py
@@ -370,7 +370,7 @@ def handle_message(user, card):
 def AI_play(table_id):
     Table = Server.active_tables[table_id]
     dummy = Table.current_game.get_partner(Table.current_game.current_bridgehand.declarer)
-    if 'Robot' in Table.current_game.current_bridgehand.players[Table.current_game.current_player] or (Table.current_game.current_player == dummy and 'Robot' in Table.current_game.current_bridgehand.players[Table.current_game.current_bridgehand.declarer]):
+    if ('Robot' in Table.current_game.current_bridgehand.players[Table.current_game.current_player] and not Table.current_game.current_player == dummy ) or (Table.current_game.current_player == dummy and 'Robot' in Table.current_game.current_bridgehand.players[Table.current_game.current_bridgehand.declarer]):
         if len(Table.current_game.current_bridgehand.play) == 0:
             card = Table.AI_opening_lead()
         else:
